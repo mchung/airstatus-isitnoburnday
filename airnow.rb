76732116f06f33776bb15ef94d1dc243a4bf0e02
Marc Chung <mchung@gmail.com>
2012-01-05 15:33:52 -0700

    Whoops

--- airnow.rb
+++
@@ -1,6 +0,0 @@
-API_KEY = "6A5A01B6-EDC6-41A0-8069-FCD8E73D4EF2"
-url = "http://ws1.airnowgateway.org/GatewayWebServiceREST/Gateway.svc/forecastbyzipcode?zipcode=94954&date=2010-01-15&format=json&key=#{API_KEY}"
-
-puts url
-
-# http://ws1.airnowgateway.org/GatewayWebServiceREST/Gateway.svc/reportingareamaxavg?name=San%20Francisco&stateid=CA&date=2011-04-26&format=json&key=6A5A01B6-EDC6-41A0-8069-FCD8E73D4EF2
